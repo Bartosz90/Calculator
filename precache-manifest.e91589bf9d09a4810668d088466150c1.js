95f4526dd13ea0a5f40cbafdb5bb4677c1fedd3f
Bartek Krupa <krupa1.bk@gmail.com>
2019-08-12 17:00:11 +0200

    Updates

---
+++ precache-manifest.e91589bf9d09a4810668d088466150c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f5c9080752a3c6d676db6eb65c5fee",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "8446b4d1ad248d663cc5",
+    "url": "/Calculator/static/css/main.024efbac.chunk.css"
+  },
+  {
+    "revision": "38d7305de38effc160fc",
+    "url": "/Calculator/static/js/2.8a9fda20.chunk.js"
+  },
+  {
+    "revision": "8446b4d1ad248d663cc5",
+    "url": "/Calculator/static/js/main.3f4edce8.chunk.js"
+  },
+  {
+    "revision": "a21fdcbbd6ecaaab2d79",
+    "url": "/Calculator/static/js/runtime~main.2c22c7e0.js"
+  }
+]);
